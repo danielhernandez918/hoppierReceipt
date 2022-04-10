09439d8b7a37740b8ac5804008d9ae097e49b9a4
Daniel Hernandez <danielhernandez918@gmail.com>
2022-04-09 22:55:40 -0700

    first commit

---
+++ src/main/java/com/daniel/hoppierReceipt/HoppierReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.hoppierReceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppierReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppierReceiptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/hoppierReceipt/HoppierReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.hoppierReceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppierReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
